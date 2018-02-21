28870d539228181987466f1de4988380a4ed4c0b
Zalán Valkó <valzalan@gmail.com>
2018-02-21 22:55:50 +0100

    Fixed some bugs

--- sudoku.js
+++ sudoku.js
@@ -1,5 +1,5 @@
 //                   Created by Zalán Valkó
-//                   Last edit: 2018.02.19
+//                   Last edit: 2018.02.21
 //  Solves the sudoku using a recursive backtracking algorithm
 
 let grid = [[0,0,0, 0,0,0, 0,0,0,],
@@ -15,12 +15,16 @@ let grid = [[0,0,0, 0,0,0, 0,0,0,],
             [0,0,0, 0,0,0, 0,0,0]];
 
 function sudokuSolver(){
-	
-	if(solve()){
-    	  	write();
-    	}else{
-      		alert("There is no solution");
-    	}
+  if(isSolvable()){
+    if(solve()){
+      write();
+    }else{
+      alert("There is no solution");
+    }
+  }else{
+    alert("There is no solution");
+  }
+}
 
   function findUnAssigned(){
     for(i=0; i<9; i++){
@@ -80,23 +84,22 @@ function sudokuSolver(){
     }
     return true;
   }
-  
+
   // utility function to print the numbers
   function write(){
     let cell, c;
-    for(i=1; i<=9; i++){
-      for(j=1; j<=9; j++){
+    for(i=0; i<9; i++){
+      for(j=0; j<9; j++){
         cell = `r${i}c${j}`;
         c = document.getElementById(cell);
-        c.value = grid[i-1][j-1];
+        c.value = grid[i][j];
       }
     }
   }
-}
 
 //placeholder function to fill in a preset -- going to be replaced
 //by a generator function in the future
-function fill(){
+function generateSudoku(){
   let grid2 = [[0,4,1, 9,0,0, 3,0,0,],
               [0,0,0, 0,0,2 ,0,0,0,],
               [7,0,0, 0,4,6, 0,0,1,],
@@ -109,15 +112,15 @@ function fill(){
               [0,0,0, 5,0,0 ,0,0,0,],
               [0,0,9, 0,0,1, 7,5,0]];
   let cell, c;
-  for(i=1; i<=9; i++){
-    for(j=1; j<=9; j++){
+  for(i=0; i<9; i++){
+    for(j=0; j<9; j++){
       cell = `r${i}c${j}`;
       c = document.getElementById(cell);
-      if(grid2[i-1][j-1] == 0){
+      if(grid2[i][j] == 0){
         c.value = "";
       }else{
-        grid[i-1][j-1] = grid2[i-1][j-1];
-        c.value = grid[i-1][j-1];
+        grid[i][j] = grid2[i][j];
+        c.value = grid[i][j];
       }
     }
   }
@@ -126,10 +129,10 @@ function fill(){
 //utility function -- assigns 0 to every cell
 function clearGrid(){
   let cell, c;
-  for(i=1; i<=9; i++){
-    for(j=1; j<=9; j++){
+  for(i=0; i<9; i++){
+    for(j=0; j<9; j++){
       cell = `r${i}c${j}`;
-      grid[i-1][j-1] = 0;
+      grid[i][j] = 0;
       c = document.getElementById(cell);
       c.value = "";
     }
@@ -142,38 +145,84 @@ function loadNumber(cell, input, field){
   let temp = cell.split("");
   let row = temp[1];
   let col = temp[3];
-  if((grid[row-1][col-1]) != 0) return false;
-  if(key == 8 && key == 46){    //user pressed backspace or delete
-    grid[row-1][col-1] = 0;
+  if(key == 8 || key == 46){    //user pressed backspace or delete
+    grid[row][col] = 0;
+    return;
   }
+  if((grid[row][col]) != 0) return;
   if(!isNaN(input.key)){
     let num = parseInt(input.key, 10);
-    grid[row-1][col-1] = num;
-	if(!checkInput(num, row, col)) alert("Contradiction!");
+    grid[row][col] = num;
+    if(checkIfSolved()){
+      alert("Congratulations! You solved it!");
+    }
+    return;
   }else{                          //input was not a number
-    grid[row-1][col-1] = 0;      //0 necessary for clarity
+    grid[row][col] = 0;      //0 necessary for clarity
     field.value = "";
+    console.log("Not a number. In cell: " + grid[row][col]);
   }
 }
 
-function checkInput(num, row, col){
-	//scanning row and column
-	for(i=0; i<9; i++){
-      	if((grid[row][i] == num && i != col) ||
-    	 	(grid[i][col] == num && i != row)){
-       	return false;
-   		}
- 	}
-    //scanning box
-    boxStartRow = (row >= 3) ? (row >= 6 ? 6 : 3) : 0;
-    boxStartCol = (col >= 3) ? (col >= 6 ? 6 : 3) : 0;
-
-    for(i=boxStartRow; i<(boxStartRow + 3); i++){
-      for(j=boxStartCol; j<(boxStartCol + 3); j++){
-        if((grid[i][j] == num) && (i != row) && (j != col)){
+function isSolvable(){
+  let row = 0; col = 0;
+  let i, j;
+  let num, boxStartRow, boxStartCol;
+  for(row = 0; row<9; row++){
+    for(i=0; i<9; i++){
+      num = grid[row][i];
+      if(num == 0) continue;
+      for(j=0; j<9; j++){
+        if(grid[row][j] == num && j != i){
           return false;
         }
       }
     }
-    return true;
+  }
+
+  for(col = 0; col<9; col++){
+    for(i=0; i<9; i++){
+      num = grid[i][col];
+      if(num == 0) continue;
+      for(j=0; j<9; j++){
+        if(grid[j][col] == num && j != i){
+          return false;
+        }
+      }
+    }
+  }
+  for(row=0; row<9; row++){
+    for(col=0; col<9; col++){
+      num = grid[row][col];
+      if(num == 0) continue;
+      boxStartRow = (row >= 3) ? (row >= 6 ? 6 : 3) : 0;
+      boxStartCol = (col >= 3) ? (col >= 6 ? 6 : 3) : 0;
+      for(i=boxStartRow; i<(boxStartRow + 3); i++){
+        for(j=boxStartCol; j<(boxStartCol + 3); j++){
+          if((grid[i][j] == num) && (i != row) && (j != col)){
+            return false;
+          }
+        }
+      }
+    }
+  }
+  return true;
+}
+
+function checkIfSolved(){
+  let num;
+  for(row=0; row<9; row++){
+    for(col=0; col<9; col++){
+      if(grid[row][col] == 0) return false;
+      num = grid[row][col];
+      let cell = new Cell(row, col);
+      if(!isSafe(num, cell)) return false;
+    }
+  }
+  return true;
+
+  function Cell(row, col){
+    this.row = row;
+    this.col = col;
+  }
 }
